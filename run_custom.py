ce7bdd12888a2581db94a6c9c4e248b8488b01ef
Manu Bansal <manub@lightup.ai>
2021-11-27 15:13:05 -0800

    own wrapper on sphinx

--- run_custom.py
+++ run_custom.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3                                                                                
 
-import speech_recognition as sr  
+import pprint
+
+#built-in
+#import speech_recognition as sr  
+
+#custom
+import custom_speech_recognition as sr
 
 # get audio from the microphone                                                                       
 r = sr.Recognizer()                                                                                   
+
 with sr.Microphone() as source:                                                                       
     print("Speak:")                                                                                   
     audio = r.listen(source)   
 
 try:
-    print("You said " + r.recognize_google(audio))
+    #print("You said " + r.recognize_google(audio))
+    #pprint.pprint(r.recognize_google(audio, show_all=True))
+    #print("You said " + r.recognize_sphinx(audio))
+    #pprint.pprint(r2.recognize_sphinx(audio, show_all=True))
+    decoder = r.recognize_sphinx(audio, show_all=True)
+    hypothesis = decoder.hyp()
+    hypstr = hypothesis.hypstr
+    pprint.pprint(hypstr)
+    segments = [(s.word, s.start_frame, s.end_frame) for s in decoder.seg()]
+    pprint.pprint(segments)
+
 except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
