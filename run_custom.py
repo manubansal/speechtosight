f0aeb0eaefd9a44b0b0a58dc14a3dd7f65d7a2dd
Manu Bansal <manub@lightup.ai>
2021-11-27 18:11:25 -0800

    refactoring for built-in sample

--- run_custom.py
+++ run_custom.py
@@ -8,21 +8,26 @@ import pprint
 #custom
 import custom_speech_recognition as sr
 from speech_recognition import AudioData
+import pocketsphinx as px
+import os
 
 # get audio from the microphone                                                                       
 r = sr.Recognizer()                                                                                   
 
-with sr.Microphone() as source:                                                                       
-    print("Speak:")                                                                                   
-    audio = r.listen(source)   
+def decode_sample(decoder):
+    # Decode streaming data
+    buf = bytearray(1024)
+    with open(os.path.join(px.get_data_path(), 'goforward.raw'), 'rb') as f:
+        decoder.start_utt()
+        while f.readinto(buf):
+            decoder.process_raw(buf, False, False)
+        decoder.end_utt()
+    #print('Best hypothesis segments:', [seg.word for seg in decoder.s
 
-try:
-    #print("You said " + r.recognize_google(audio))
-    #pprint.pprint(r.recognize_google(audio, show_all=True))
-    #print("You said " + r.recognize_sphinx(audio))
-    #pprint.pprint(r2.recognize_sphinx(audio, show_all=True))
-    #decoder = r.recognize_sphinx(audio, show_all=True)
-    decoder = r.build_decoder()
+def decode_capture(decoder):
+    with sr.Microphone() as source:                                                                       
+        print("Speak:")                                                                                   
+        audio = r.listen(source)   
 
     assert isinstance(audio, AudioData), "``audio_data`` must be audio data"
 
@@ -32,6 +37,19 @@ try:
     decoder.start_utt()  # begin utterance processing
     decoder.process_raw(raw_data, False, True)  # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
     decoder.end_utt()  # stop utterance processing
+
+
+try:
+    #print("You said " + r.recognize_google(audio))
+    #pprint.pprint(r.recognize_google(audio, show_all=True))
+    #print("You said " + r.recognize_sphinx(audio))
+    #pprint.pprint(r2.recognize_sphinx(audio, show_all=True))
+    #decoder = r.recognize_sphinx(audio, show_all=True)
+    decoder = r.build_decoder()
+
+    decode_sample(decoder)
+    #decode_capture(decoder)
+
 
     hypothesis = decoder.hyp()
     hypstr = hypothesis.hypstr
